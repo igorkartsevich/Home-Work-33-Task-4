e6e406e8cc3c9c8585717923ec93bfebb8790ef7
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2022-03-13 13:48:29 +0300

    13/03/01

--- Home-Work-33-Task-4/Home-Work-33-Task-4.cpp
+++ Home-Work-33-Task-4/Home-Work-33-Task-4.cpp
@@ -1,7 +1,36 @@
 ﻿#include <iostream>
 
+template<typename T>
+void loadData(T data[5]) {
+    T data_enter;
+    for (int i = 0; i < 5; ++i) {
+        std::cin >> data_enter;
+        data[i] = data_enter;
+    }
+}
+
+template<typename T>
+T average(T data[5]) {
+    T sum = 0;
+    for (int i = 0; i < 5; ++i) {
+        sum += data[i];
+    }
+    return sum / 5;
+}
+
 int main()
 {
+    int data_int[5];
+    loadData(data_int);
+    std::cout << "Average: " << average(data_int) << std::endl;
+
+    double data_dbl[5];
+    loadData(data_dbl);
+    std::cout << "Average: " << average(data_dbl) << std::endl;
+
+    float data_flt[5];
+    loadData(data_flt);
+    std::cout << "Average: " << average(data_flt) << std::endl;
 
     return 0;
 }
